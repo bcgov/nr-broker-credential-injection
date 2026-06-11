a7af11d7742ebe596fbad7868c37f5d0954b57e2
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2026-06-11 16:20:00 -0700

    fix: url env

--- provision-secret/src/get-vault-secret.sh
+++ provision-secret/src/get-vault-secret.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Validate required environment variables
-BROKER_URL="${BROKER_URL:-https://broker.io.nrs.gov.bc.ca}"
+BROKER_ADDR="${BROKER_ADDR:-https://broker.io.nrs.gov.bc.ca}"
 VAULT_ADDR="${VAULT_ADDR:-https://knox.io.nrs.gov.bc.ca}"
 OPENSHIFT_SECRET_NAME="${OPENSHIFT_SECRET_NAME:-vault-secret}"
 OPENSHIFT_TOKEN_KEY="${OPENSHIFT_TOKEN_KEY:-token}"
@@ -18,7 +18,7 @@ cleanup() {
     EXIT_CODE=$?
     if [ "$EXIT_CODE" -ne 0 ] && [ -n "$INTENTION_TOKEN" ]; then
         echo "=> Intention: close (failure)"
-        curl -s -X POST "$BROKER_URL/v1/intention/close?outcome=failure" -H "X-Broker-Token: $INTENTION_TOKEN" || true
+        curl -s -X POST "$BROKER_ADDR/v1/intention/close?outcome=failure" -H "X-Broker-Token: $INTENTION_TOKEN" || true
     fi
 }
 trap cleanup EXIT
@@ -26,10 +26,10 @@ set -e
 
 PROVISION_NAME=$(jq -r '.actions[0].id' ./config/intention.json)
 
-echo "=> Intention: open ($BROKER_URL)"
+echo "=> Intention: open ($BROKER_ADDR)"
 
 # Open intention
-RESPONSE=$(curl -s -X POST "$BROKER_URL/v1/intention/open" \
+RESPONSE=$(curl -s -X POST "$BROKER_ADDR/v1/intention/open" \
     -H 'Content-Type: application/json' \
     -H "Authorization: Bearer $BROKER_JWT" \
     -d "$(jq --arg event_url "$INTENTION_EVENT_URL" --arg user "${INTENTION_USER:-}" --arg env "$INTENTION_ENVIRONMENT" \
@@ -48,11 +48,11 @@ ACTION_TOKEN=$(echo "$RESPONSE" | jq -r ".actions.$PROVISION_NAME.token")
 
 echo "=> Action: start"
 # Start action
-curl -s -X POST "$BROKER_URL/v1/intention/action/start" -H "X-Broker-Token: $ACTION_TOKEN"
+curl -s -X POST "$BROKER_ADDR/v1/intention/action/start" -H "X-Broker-Token: $ACTION_TOKEN"
 
 # Get wrapped id for access
 echo "Get wrapped vault token from broker"
-VAULT_TOKEN_WRAP=$(curl -s -X POST "$BROKER_URL/v1/provision/approle/secret-id" \
+VAULT_TOKEN_WRAP=$(curl -s -X POST "$BROKER_ADDR/v1/provision/approle/secret-id" \
     -H "X-Broker-Token: $ACTION_TOKEN" \
     -H "X-Vault-Role-Id: $VAULT_ROLE_ID")
 WRAPPED_VAULT_TOKEN=$(echo "$VAULT_TOKEN_WRAP" | jq -r '.wrap_info.token')
@@ -73,8 +73,8 @@ echo "Success! Secret ID saved to OpenShift secret $OPENSHIFT_SECRET_NAME"
 
 echo "=> Action: end"
 # End action
-curl -s -X POST "$BROKER_URL/v1/intention/action/end" -H "X-Broker-Token: $ACTION_TOKEN"
+curl -s -X POST "$BROKER_ADDR/v1/intention/action/end" -H "X-Broker-Token: $ACTION_TOKEN"
 
 echo "=> Intention: close"
 # Use saved intention token to close intention
-curl -s -X POST "$BROKER_URL/v1/intention/close" -H "X-Broker-Token: $INTENTION_TOKEN"
+curl -s -X POST "$BROKER_ADDR/v1/intention/close" -H "X-Broker-Token: $INTENTION_TOKEN"
